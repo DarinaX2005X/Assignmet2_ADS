3f6c22d46cd2f117c616ec7a24896ea7c25d2440
Darina <darinax2005x@gmail.com>
2024-04-21 20:20:05 +0500

    Third commit: implemented methods for LinkedList

---
+++ src/MyLinkedList.java
@@ -0,0 +1,91 @@
+public class MyLinkedList<T> implements MyList<T> {
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public void add(int index, T item) {
+
+    }
+
+    @Override
+    public void set(int index, T item) {
+
+    }
+
+    @Override
+    public void addFirst(T item) {
+
+    }
+
+    @Override
+    public void addLast(T item) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getFirst() {
+        return null;
+    }
+
+    @Override
+    public T getLast() {
+        return null;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void removeFirst() {
+
+    }
+
+    @Override
+    public void removeLast() {
+
+    }
+
+    @Override
+    public void sort(Comparator<T> cmp) {
+
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean exists(java.lang.Object object) {
+        return false;
+    }
+
+    @Override
+    public int lastIndexOf(java.lang.Object object) {
+        return 0;
+    }
+
+    @Override
+    public int indexOf(java.lang.Object object) {
+        return 0;
+    }
+}
